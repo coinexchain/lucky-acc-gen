40cfe2fe99df5be3c35788ecf23c9b09711cb01b
wangkui0508 <wangkui0508@hotmail.com>
2019-09-01 23:18:03 +0800

    move function to "coinex" object

--- trywasm/main.go
+++ trywasm/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	js.Global().Get("document").Set("wasmFunc", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+	js.Global().Get("document").Get("coinex").Set("wasmFunc1", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
 		js.Global().Get("window").Call("fromWasm", fmt.Sprintf("insidewasm-%s", args[0].String()))
 		return nil
 	}))

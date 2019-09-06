9877b3d9bf9aef7e215931547471dc2b2f2dba4e
wangkui0508 <wangkui0508@hotmail.com>
2019-09-06 10:54:05 +0800

    Move some logic to polarbear.

--- accgen/accgen.go
+++ accgen/accgen.go
@@ -5,12 +5,10 @@ import (
 	"sync"
 	"strings"
 	"sync/atomic"
-	"github.com/cosmos/cosmos-sdk/crypto/keys/hd"
-	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	bip39 "github.com/cosmos/go-bip39"
 
-	"github.com/tendermint/tendermint/crypto/secp256k1"
+	"github.com/coinexchain/polarbear/keybase"
 	"golang.org/x/crypto/blake2b"
 )
 
@@ -61,7 +59,7 @@ func tryAddress(prefix string, resAtomic atomic.Value, wg *sync.WaitGroup, globa
 				fmt.Printf("%d times have been tried, estimated progress: %.2f%%\n", count, percent)
 			}
 		}
-		addr, mnemonic, err := getAddress(entropy)
+		addr, mnemonic, err := keybase.GetAddressFromEntropy(entropy)
 		if err != nil {
 			panic(err.Error())
 		}
@@ -78,37 +76,5 @@ func tryAddress(prefix string, resAtomic atomic.Value, wg *sync.WaitGroup, globa
 		counter++
 	}
 	wg.Done()
-}
-
-// NewFundraiserParams creates a BIP 44 parameter object from the params:
-// m / 44' / coinType' / account' / 0 / address_index
-// The fixed parameters (purpose', coin_type', and change) are determined by what was used in the fundraiser.
-//func NewFundraiserParams(account, coinType, addressIdx uint32) *BIP44Params {
-//	return NewParams(44, coinType, account, false, addressIdx)
-//}
-
-func getAddress(entropy []byte) (string,string,error) {
-	mnemonic, err := bip39.NewMnemonic(entropy)
-	if err != nil {
-		return "", mnemonic, err
-	}
-
-	DefaultBIP39Passphrase := ""
-	seed := bip39.NewSeed(mnemonic, DefaultBIP39Passphrase)
-	fullHdPath := "44'/688'/0'/0/0" //coinType=688 account=0 addressIdx=0
-	masterPriv, ch := hd.ComputeMastersFromSeed(seed)
-	derivedPriv, err := hd.DerivePrivateKeyForPath(masterPriv, ch, fullHdPath)
-	pubk := secp256k1.PrivKeySecp256k1(derivedPriv).PubKey()
-	addr := pubk.Address()
-	acc := sdk.AccAddress(addr)
-	return acc.String(), mnemonic, nil
-}
-
-func getRandAddress() (string,string,error) {
-	entropy, err := bip39.NewEntropy(32*4)
-	if err != nil {
-		return "", "", err
-	}
-	return getAddress(entropy)
 }
 
